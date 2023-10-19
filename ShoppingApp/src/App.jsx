443a33a098e25f0d24af8ee8e3e0f0d855b5ce6f
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-19 18:08:34 +0530

    First Commit

---
+++ ShoppingApp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Component/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
