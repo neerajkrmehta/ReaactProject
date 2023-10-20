68341f8b6988cb99b6acd5f8e3c69cf9c98ce472
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-10-20 19:32:36 +0530

    Add some Componets

---
+++ ShoppingApp/src/Component/NewCollections/NewCollections.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './NewCollections.css'
+import new_collection from '../Assets/new_collections'
+import new_collections from '../Assets/new_collections'
+import { Item } from '../Item/Item'
+
+export const NewCollections = () => {
+  return (
+    <div className='new-collections'>
+    <p>New Collections</p>
+    <hr />
+    <div className="collections">
+    {new_collections.map((item,i)=>{
+    return (
+      <Item
+        key={i}
+        id={item.id}
+        name={item.name}
+        image={item.image}
+        new_price={item.new_price}
+        old_price={item.old_price}
+      />
+    );
+    })}
+    </div>
+    </div>
+  )
+}
--- ShoppingApp/src/Pages/Shop.jsx
+++ ShoppingApp/src/Pages/Shop.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Hero } from '../Component/Hero/Hero'
 import { Popular } from '../Component/Popular/Popular'
+import { Offers } from '../Component/Offers/Offers'
+import { NewCollections } from '../Component/NewCollections/NewCollections'
+import { NewsLetter } from '../Component/NewsLetter/NewsLetter'
 
 export const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
